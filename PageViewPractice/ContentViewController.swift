3ae7c97154dcbb9ac37eba809bd97e0a426d8a37
K Sabbak <ksabbak@gmail.com>
2016-04-18 16:24:57 -0500

    Basic set-up

---
+++ PageViewPractice/ContentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ContentViewController.swift
+//  PageViewPractice
+//
+//  Created by K Sabbak on 4/18/16.
+//  Copyright © 2016 ksabbak. All rights reserved.
+//
+// This is the VC that handles the content displayed on the PageViewControllers Views.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
---
+++ PageViewPractice/PageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PageViewController.swift
+//  PageViewPractice
+//
+//  Created by K Sabbak on 4/18/16.
+//  Copyright © 2016 ksabbak. All rights reserved.
+//
+//  This is all the code that needs to be handled by the UIPageViewController
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
